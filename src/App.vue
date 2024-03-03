82b134f7ae37ea67167afca7cb70aa2f25746ce3
jin <dlwlstjr0109@gmail.com>
2024-03-03 16:19:41 +0900

    반복문

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
   <HelloWorld msg="Welcome to Your Vue.js App"/>
   <dev>
     <h4>{{ status }}</h4>
+    <a v-for="i in data" :key="i">{{ i }}입니다</a>
   </dev>
 </template>
 
@@ -14,6 +15,7 @@ export default {
   data(){
     return{
       status:"안녕하세요 처음뵙겠습니다",
+      data:["A","B","C"]
     }
   },
   components: {
